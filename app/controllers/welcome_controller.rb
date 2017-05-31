5a2b7347974bf50ce0c7772df75687f51d894730
Constance Gobert <constancegobert@hotmail.fr>
2017-05-31 14:05:03 +0200

    ch12 static routes completed

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,9 @@
+
+# file automatically generated (with about.erb & index.erb) with command: "rails generate controller welcome index about"
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+end
