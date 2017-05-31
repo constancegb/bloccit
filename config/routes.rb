47274610f37e54386d68fa130f22eeab2e0f6569
Constance Gobert <constancegobert@hotmail.fr>
2017-05-31 15:55:30 +0200

    assignment 13 completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
-  root 'welcome#index' #sets default home page 
+  get 'welcome/faq'
+
+  root 'welcome#index' #sets default home page
 
 end
