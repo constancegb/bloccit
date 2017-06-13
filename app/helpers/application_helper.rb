93b91ab1d01b49979f98bf2d8c64689dabb4d8a2
Constance Gobert <constancegobert@hotmail.fr>
2017-06-13 14:42:44 +0200

    checkpoint 23 completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def form_group_tag(errors, &block) #PS: errors is an array || & turns the block into a Proc(reusable block like a variable)
+     css_class = 'form-group'
+     css_class << ' has-error' if errors.any?
+
+     content_tag :div, capture(&block), class: css_class #build HTML & CSS to display form element & associated errors.
+   end
 end
