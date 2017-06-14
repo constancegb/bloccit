615bbe419d340d76b7da977ca9b1f202d43b5dd0
Constance Gobert <constancegobert@hotmail.fr>
2017-06-14 14:58:54 +0200

    assignment 26 add avatar done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,15 @@
 module ApplicationHelper
+
   def form_group_tag(errors, &block) #PS: errors is an array || & turns the block into a Proc(reusable block like a variable)
      css_class = 'form-group'
      css_class << ' has-error' if errors.any?
 
      content_tag :div, capture(&block), class: css_class #build HTML & CSS to display form element & associated errors.
    end
+
+   def avatar_url(user)
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+   end
+   
 end
